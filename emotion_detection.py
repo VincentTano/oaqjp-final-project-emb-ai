2bc4e91427e4051a5a29b9edade3752227bb0059
VincentTano <vincent_tano@hotmail.com>
2026-02-11 10:33:28 -0500

    Task 3

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,4 @@
-import requests
+import requests, json
 
 def emotion_detector(text_to_analyse: str = ""):
     url='https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
@@ -6,4 +6,8 @@ def emotion_detector(text_to_analyse: str = ""):
     input_json={ "raw_document": { "text": text_to_analyse } }
 
     response = requests.post(url, json=input_json, headers=headers)
-    return(response.text)
+    result = json.loads(response.text)
+    emotion = result['emotionPredictions'][0]['emotion']
+    emotion['dominant_emotion']=sorted(emotion,key=emotion.get,reverse=True)[0]
+
+    return(emotion)
